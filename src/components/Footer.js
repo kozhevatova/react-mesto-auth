3444aa052056986272aad9cd88e46736fcdacbe3
Anna Kozhevatova <ad.kozhevatova@yandex.ru>
2021-02-03 16:00:51 +0300

    Updates

--- src/components/Footer.js
+++
@@ -1,9 +0,0 @@
-const Footer = () => {
-    return (
-        <footer className="footer">
-            <p className="footer__copyright">&copy; <span id="year">{new Date().getFullYear()}</span> Mesto Russia</p>
-        </footer>
-    );
-}
-
-export default Footer;
